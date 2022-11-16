1dc63b59745985f738067e20d7536acef0b39b97
iytang <iytang@outlook.com>
2022-11-16 08:50:14 +0800

    P2PChat

---
+++ P2PChat/P2PChat/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace P2PChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rtbReceive = new System.Windows.Forms.RichTextBox();
+            this.rtbSend = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIPAddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNickName = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // rtbReceive
+            // 
+            this.rtbReceive.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbReceive.Location = new System.Drawing.Point(12, 12);
+            this.rtbReceive.Name = "rtbReceive";
+            this.rtbReceive.Size = new System.Drawing.Size(599, 262);
+            this.rtbReceive.TabIndex = 0;
+            this.rtbReceive.Text = "";
+            // 
+            // rtbSend
+            // 
+            this.rtbSend.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbSend.Location = new System.Drawing.Point(12, 280);
+            this.rtbSend.Name = "rtbSend";
+            this.rtbSend.Size = new System.Drawing.Size(599, 116);
+            this.rtbSend.TabIndex = 1;
+            this.rtbSend.Text = "";
+            this.rtbSend.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.rtbSend_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 419);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "对方主机：";
+            // 
+            // txtIPAddress
+            // 
+            this.txtIPAddress.Location = new System.Drawing.Point(85, 416);
+            this.txtIPAddress.Name = "txtIPAddress";
+            this.txtIPAddress.Size = new System.Drawing.Size(149, 21);
+            this.txtIPAddress.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 419);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "昵称：";
+            // 
+            // txtNickName
+            // 
+            this.txtNickName.Location = new System.Drawing.Point(292, 416);
+            this.txtNickName.Name = "txtNickName";
+            this.txtNickName.Size = new System.Drawing.Size(102, 21);
+            this.txtNickName.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(409, 416);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(57, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "清屏";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(482, 415);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(57, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(554, 416);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(57, 23);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "关闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtNickName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIPAddress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbSend);
+            this.Controls.Add(this.rtbReceive);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "点对点聊天";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbReceive;
+        private System.Windows.Forms.RichTextBox rtbSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIPAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNickName;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ P2PChat/P2PChat/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P2PChat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Thread td;
+        private TcpListener tcpListener;
+        private static string message = "";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            td = new Thread(StartListen);
+            td.Start(); // 启动监听线程
+            timer1.Start();
+        }
+        private void StartListen()
+        {
+            message = "";
+            tcpListener = new TcpListener(888);
+            tcpListener.Start(); // 启动监听
+            while (true)
+            {
+                TcpClient tcpClient = tcpListener.AcceptTcpClient(); // 接受连接请求
+                NetworkStream nStream = tcpClient.GetStream(); // 获取数据流
+                byte[] buffer = new byte[1024]; // 建立缓存
+                int i = nStream.Read(buffer, 0, buffer.Length); // 将数据流写入缓存
+                message = Encoding.Default.GetString(buffer,0,i); // 将字节解码为string字符串
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (this.tcpListener != null)
+            {
+                tcpListener.Stop(); // 停止监听
+            }
+            if (td != null)
+            {
+                if (td.ThreadState == ThreadState.Running)
+                {
+                    td.Abort(); // 停止线程
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            rtbReceive.Clear(); // 清除聊天文本框
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPAddress[] ip = Dns.GetHostAddresses(Dns.GetHostName()); // 获取本机IP地址
+                // 发送的消息
+                string strMsg = $"{txtNickName.Text}({ip[0].ToString()}) {DateTime.Now.ToString("T")}\n {rtbSend.Text}\n";
+                TcpClient tcpClient = new TcpClient(txtIPAddress.Text, 888); // 建立TCP连接
+                NetworkStream networkStream = tcpClient.GetStream(); // 获取数据流
+                StreamWriter writer = new StreamWriter(networkStream,Encoding.Default); // 数据流写入实例
+                writer.Write(strMsg);
+                writer.Flush(); // 清理缓冲区
+                writer.Close(); // 关闭
+                rtbReceive.AppendText(strMsg);
+                rtbReceive.ScrollToCaret();
+                rtbSend.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void rtbSend_KeyPress(object sender, KeyPressEventArgs e) // 键盘事件
+        {
+            if (e.KeyChar == '\r') // 按下回车键
+            {
+                btnSend.PerformClick(); // 触发发送事件
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (message != "")
+            {
+                rtbReceive.AppendText(message);
+                rtbReceive.ScrollToCaret();
+                message = ""; // 初始化message
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // 退出程序
+        }
+    }
+}
